f3e2e3216081d67cf69a8d19532ef1fa4ae7df2e
jeffreyally <jeffreyally@gmail.com>
2022-02-12 23:58:02 +0000

    PUT added

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -13,23 +13,49 @@ export const TodoList = (props) => {
 			})
 			.then((responseinJS) => {
 				modifyTask(responseinJS);
-				console.log("Response in JS", responseinJS);
 			});
 	}, []);
+
+	const putRequest = (updatedTodos) => {
+		//updatedTodos is just a placeholder for the info we get from newToDos becuase newToDO only exist in saveTask function
+		//put  FETCH API command
+		fetch(URI, {
+			method: "PUT",
+			body: JSON.stringify(updatedTodos), // data can be `string` or {object}!
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				// from class notes ----WOULD I USE THROW ERROR HERE ???
+				// from class notes ----if(!resp.ok){
+				//	from class notes ----throw Error(resp.statusText)}
+				//console.log(resp.ok); // will be true if the response is successfull
+				//console.log(resp.status); // the status code = 200 or code = 400 etc.
+				//console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then((data) => {
+				// from class notes ----console.log("success", response)
+				// from class notes ----getFetch()
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch((error) => {
+				//error handling
+				console.log(error);
+				//from class notes ----console.error("error", error);
+			});
+	};
 
 	const [inputTask, setInputTask] = useState("");
 	const [taskList, modifyTask] = useState([]);
 	//update modifyTask to setTaskList in the future
 	let saveTask = (e) => {
 		if (e.keyCode == 13) {
-			let todos = [...taskList];
-			let newTodos = todos.concat({
-				// takes previous list and new item to make current list
-				label: inputTask,
-				done: false,
-			});
-			console.log(newTodos);
-			modifyTask(newTodos);
+			modifyTask([...taskList, { label: inputTask, done: false }]);
+			putRequest([...taskList, { label: inputTask, done: false }]);
+
 			setInputTask("");
 		}
 	};
